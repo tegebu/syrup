d0fcb7dd8d57e8f32025203735b029fc6d245213
jamashita <jamashita@arachnid.xyz>
2020-09-27 11:13:16 +0900

    feat: 🎸 Language

---
+++ src/VO/Language/Language.ts
@@ -0,0 +1,67 @@
+import { JSONable } from '@jamashita/publikum-interface';
+import { ValueObject } from '@jamashita/publikum-object';
+import { LanguageID } from './LanguageID';
+import { LanguageName } from './LanguageName';
+
+// TODO iso639
+export type LanguageJSON = Readonly<{
+  id: string;
+  name: string;
+}>;
+
+export class Language extends ValueObject<'Language'> implements JSONable<LanguageJSON> {
+  public readonly noun: 'Language' = 'Language';
+  private readonly id: LanguageID;
+  private readonly name: LanguageName;
+
+  public static of(id: LanguageID, name: LanguageName): Language {
+    return new Language(id, name);
+  }
+
+  protected constructor(id: LanguageID, name: LanguageName) {
+    super();
+    this.id = id;
+    this.name = name;
+  }
+
+  public equals(other: unknown): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (!(other instanceof Language)) {
+      return false;
+    }
+    if (!this.id.equals(other.id)) {
+      return false;
+    }
+    if (!this.name.equals(other.name)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public serialize(): string {
+    const properties: Array<string> = [];
+
+    properties.push(this.id.toString());
+    properties.push(this.name.toString());
+
+    return properties.join(', ');
+  }
+
+  public toJSON(): LanguageJSON {
+    return {
+      id: this.id.get().get(),
+      name: this.name.get()
+    };
+  }
+
+  public getLanguageID(): LanguageID {
+    return this.id;
+  }
+
+  public getLanguageName(): LanguageName {
+    return this.name;
+  }
+}
