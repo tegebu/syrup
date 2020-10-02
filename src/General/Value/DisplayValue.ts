8574879279b83b50f72206dc02eed7ce51880afb
jamashita <jamashita@arachnid.xyz>
2020-10-02 16:06:53 +0900

    feat: 🎸 interface separeted into 2

--- src/General/Value/DisplayValue.ts
+++ src/General/Value/DisplayValue.ts
@@ -1,10 +1,9 @@
 import { Nominative } from '@jamashita/publikum-interface';
+import { Displayable } from './Displayable';
 
 export type ValueType = 'unique' | 'range';
 
-export interface DisplayValue<V extends ValueType = ValueType, N extends string = string> extends Nominative<N> {
+export interface DisplayValue<V extends ValueType = ValueType, N extends string = string> extends Displayable, Nominative<N> {
   readonly noun: N;
   readonly type: V;
-
-  display(): string;
 }
