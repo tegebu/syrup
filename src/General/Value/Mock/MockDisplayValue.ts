ed02196dc8f7d63ca706e8b9d8d0c24592095631
jamashita <jamashita@arachnid.xyz>
2020-10-02 16:48:44 +0900

    test: 💍 mock

--- src/General/Value/Mock/MockDisplayValue.ts
+++ src/General/Value/Mock/MockDisplayValue.ts
@@ -1,19 +1,20 @@
 import { UnimplementedError } from '@jamashita/publikum-error';
+import { ValueObject } from '@jamashita/publikum-object';
 import { DisplayValue } from '../DisplayValue';
 
-export class MockDisplayValue implements DisplayValue<'none', 'MockDisplayValue'> {
+export class MockDisplayValue extends ValueObject<'MockDisplayValue'> implements DisplayValue<'none', 'MockDisplayValue'> {
   public readonly noun: 'MockDisplayValue' = 'MockDisplayValue';
   public readonly type: 'none' = 'none';
+
+  public constructor() {
+    super();
+  }
 
   public display(): string {
     throw new UnimplementedError();
   }
 
   public equals(): boolean {
-    throw new UnimplementedError();
-  }
-
-  public hashCode(): string {
     throw new UnimplementedError();
   }
 
