964879ac1d40e5870a467da28e2fac842098708a
jamashita <jamashita@arachnid.xyz>
2020-10-03 12:07:56 +0900

    feat: 🎸 RangeValue accepts NumericalValue

--- src/General/ValueRange/__tests__/RangeValue.spec.ts
+++ src/General/ValueRange/__tests__/RangeValue.spec.ts
@@ -1,5 +1,6 @@
 import { MockValueObject } from '@jamashita/publikum-object';
-import { ValueError } from '../../Value/Error/ValueError';
+import { BareValue } from '../../Value/BareValue';
+import { ValueRangeError } from '../Error/ValueRangeError';
 import { RangeValue } from '../RangeValue';
 
 describe('RangeValue', () => {
@@ -7,99 +8,41 @@ describe('RangeValue', () => {
     it('returns its instance', () => {
       expect.assertions(6);
 
-      const value1: number = -1;
-      const value2: number = 0;
-      const value3: number = 1;
+      const value1: BareValue = BareValue.of(-1);
+      const value2: BareValue = BareValue.of(0);
+      const value3: BareValue = BareValue.of(1);
 
       const range1: RangeValue = RangeValue.of(value1, value2);
       const range2: RangeValue = RangeValue.of(value2, value3);
       const range3: RangeValue = RangeValue.of(value1, value3);
 
-      expect(range1.getMin()).toBe(value1);
-      expect(range1.getMax()).toBe(value2);
-      expect(range2.getMin()).toBe(value2);
-      expect(range2.getMax()).toBe(value3);
-      expect(range3.getMin()).toBe(value1);
-      expect(range3.getMax()).toBe(value3);
-    });
-
-    it('throws ValueError when decimal number given', () => {
-      expect.assertions(6);
-
-      const value1: number = -1.01;
-      const value2: number = 0.1;
-      const value3: number = 1.0902099;
-      const value4: number = 100;
-      const value5: number = -100;
-
-      expect(() => {
-        RangeValue.of(value1, value4);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value2, value4);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value3, value4);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value5, value1);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value5, value2);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value5, value3);
-      }).toThrow(ValueError);
+      expect(range1.getMin()).toBe(value1.get());
+      expect(range1.getMax()).toBe(value2.get());
+      expect(range2.getMin()).toBe(value2.get());
+      expect(range2.getMax()).toBe(value3.get());
+      expect(range3.getMin()).toBe(value1.get());
+      expect(range3.getMax()).toBe(value3.get());
     });
 
     it('throws ValueError when min and max are equal', () => {
       expect.assertions(1);
 
-      const value1: number = -1.01;
+      const value1: BareValue = BareValue.of(-1.01);
 
       expect(() => {
         RangeValue.of(value1, value1);
-      }).toThrow(ValueError);
+      }).toThrow(ValueRangeError);
     });
 
     it('throws ValueError when min is greater than max', () => {
       expect.assertions(1);
 
-      const value1: number = 1.0902099;
-      const value2: number = -1.01;
+      const value1: BareValue = BareValue.of(1.0902099);
+      const value2: BareValue = BareValue.of(-1.01);
 
       expect(() => {
         RangeValue.of(value1, value2);
-      }).toThrow(ValueError);
-    });
-
-    it('throws ValueError when Not a Number given', () => {
-      expect.assertions(6);
-
-      const value1: number = Infinity;
-      const value2: number = -Infinity;
-      const value3: number = NaN;
-      const value4: number = 100;
-      const value5: number = -100;
-
-      expect(() => {
-        RangeValue.of(value2, value1);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value2, value4);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value2, value5);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value4, value1);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value3, value4);
-      }).toThrow(ValueError);
-      expect(() => {
-        RangeValue.of(value5, value3);
-      }).toThrow(ValueError);
+      }).toThrow(ValueRangeError);
     });
   });
 
@@ -107,7 +50,7 @@ describe('RangeValue', () => {
     it('returns true when the same instance given', () => {
       expect.assertions(1);
 
-      const value: RangeValue = RangeValue.of(9, 10);
+      const value: RangeValue = RangeValue.of(BareValue.of(9), BareValue.of(10));
 
       expect(value.equals(value)).toBe(true);
     });
@@ -115,7 +58,7 @@ describe('RangeValue', () => {
     it('returns false when the different class instance given', () => {
       expect.assertions(1);
 
-      const value: RangeValue = RangeValue.of(9, 10);
+      const value: RangeValue = RangeValue.of(BareValue.of(9), BareValue.of(10));
 
       expect(value.equals(new MockValueObject('mock'))).toBe(false);
     });
@@ -123,10 +66,10 @@ describe('RangeValue', () => {
     it('returns true when all the properties are the same', () => {
       expect.assertions(3);
 
-      const value1: RangeValue = RangeValue.of(9, 10);
-      const value2: RangeValue = RangeValue.of(8, 10);
-      const value3: RangeValue = RangeValue.of(9, 11);
-      const value4: RangeValue = RangeValue.of(9, 10);
+      const value1: RangeValue = RangeValue.of(BareValue.of(9), BareValue.of(10));
+      const value2: RangeValue = RangeValue.of(BareValue.of(8), BareValue.of(10));
+      const value3: RangeValue = RangeValue.of(BareValue.of(9), BareValue.of(11));
+      const value4: RangeValue = RangeValue.of(BareValue.of(9), BareValue.of(10));
 
       expect(value1.equals(value2)).toBe(false);
       expect(value1.equals(value3)).toBe(false);
@@ -138,12 +81,12 @@ describe('RangeValue', () => {
     it('returns its retaining value as string', () => {
       expect.assertions(1);
 
-      const v1: number = 9;
-      const v2: number = 10;
+      const v1: BareValue = BareValue.of(9);
+      const v2: BareValue = BareValue.of(10);
 
       const value: RangeValue = RangeValue.of(v1, v2);
 
-      expect(value.toString()).toBe(`${v1}, ${v2}`);
+      expect(value.toString()).toBe(`${v1.toString()}, ${v2.toString()}`);
     });
   });
 
@@ -151,12 +94,12 @@ describe('RangeValue', () => {
     it('returns its retaining value as string', () => {
       expect.assertions(1);
 
-      const v1: number = 9;
-      const v2: number = 10;
+      const v1: BareValue = BareValue.of(9);
+      const v2: BareValue = BareValue.of(10);
 
       const value: RangeValue = RangeValue.of(v1, v2);
 
-      expect(value.display()).toBe(`${v1} - ${v2}`);
+      expect(value.display()).toBe(`${v1.toString()} - ${v2.toString()}`);
     });
   });
 });
