26b5d70d4dc72235e13fca14bf3f9eca020aaeed
jamashita <jamashita@arachnid.xyz>
2020-10-13 22:18:49 +0900

    test: 💍 mock udpate

--- src/General/Tree/Mock/MockTreeNode.ts
+++ src/General/Tree/Mock/MockTreeNode.ts
@@ -1,9 +1,9 @@
-import { ReadonlyAddress } from '@jamashita/publikum-collection';
+import { ImmutableAddress, ReadonlyAddress } from '@jamashita/publikum-collection';
 import { Nominative } from '@jamashita/publikum-interface';
 import { TreeNode } from '../TreeNode';
 
 export class MockTreeNode<V extends Nominative> extends TreeNode<V> {
-  public constructor(value: V, children: ReadonlyAddress<TreeNode<V>>) {
+  public constructor(value: V, children: ReadonlyAddress<TreeNode<V>> = ImmutableAddress.empty<TreeNode<V>>()) {
     super(value, children);
   }
 }
