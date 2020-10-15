f5c7d0329a3751b16c19ad80984dd344769afe4f
jamashita <jamashita@arachnid.xyz>
2020-10-15 21:55:39 +0900

    test: 💍 mock

---
+++ src/General/Tree/Mock/MockTreeID.ts
@@ -0,0 +1,24 @@
+import { UnimplementedError } from '@jamashita/publikum-error';
+import { ValueObject } from '@jamashita/publikum-object';
+import { Primitive } from '@jamashita/publikum-type';
+import { TreeID } from '../Interface/TreeID';
+
+export class MockTreeID extends ValueObject<'MockTreeID'> implements TreeID<Primitive, 'MockTreeID'> {
+  public readonly noun: 'MockTreeID' = 'MockTreeID';
+
+  public equals(): boolean {
+    throw new UnimplementedError();
+  }
+
+  public get(): Primitive {
+    throw new UnimplementedError();
+  }
+
+  public hashCode(): string {
+    throw new UnimplementedError();
+  }
+
+  public serialize(): string {
+    throw new UnimplementedError();
+  }
+}
