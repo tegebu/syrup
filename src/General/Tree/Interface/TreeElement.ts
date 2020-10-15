5b3dd1596f49be337288335b131d2f121a3421b0
jamashita <jamashita@arachnid.xyz>
2020-10-15 23:42:26 +0900

    test: 💍 mocking

--- src/General/Tree/Interface/TreeElement.ts
+++ src/General/Tree/Interface/TreeElement.ts
@@ -1,12 +1,15 @@
 import { ReadonlyAddress } from '@jamashita/publikum-collection';
 import { Nominative } from '@jamashita/publikum-interface';
 import { Primitive } from '@jamashita/publikum-type';
-import { TreeObject } from './TreeObject';
+import { TreeID } from './TreeID';
 
-export interface TreeElement<P extends Primitive, V extends Nominative, N extends string = string> extends TreeObject<P, N> {
+export interface TreeElement<V extends Nominative, P extends Primitive = Primitive, N extends string = string> extends Nominative<N> {
+
+  getTreeID(): TreeID<P>;
+
   getValue(): V;
 
-  getChildren(): ReadonlyAddress<TreeElement<P, V>>;
+  getChildren(): ReadonlyAddress<TreeElement<V, P>>;
 
   isLeaf(): boolean;
 }
