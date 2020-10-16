76a273ddbb0aa6f351e4350c8f48d4feaef33bbe
jamashita <jamashita@arachnid.xyz>
2020-10-16 11:11:26 +0900

    test: 💍 mock generics changed

--- src/General/Tree/Mock/MockTree.ts
+++ src/General/Tree/Mock/MockTree.ts
@@ -1,10 +1,9 @@
-import { Primitive } from '@jamashita/publikum-type';
+import { TreeElement } from '../Interface/TreeElement';
 import { TreeObject } from '../Interface/TreeObject';
 import { Tree } from '../Tree';
-import { TreeNode } from '../TreeNode';
 
-export class MockTree<P extends Primitive, V extends TreeObject<P>> extends Tree<P, V> {
-  public constructor(root: TreeNode<P, V>) {
+export class MockTree<V extends TreeObject> extends Tree<V> {
+  public constructor(root: TreeElement<V>) {
     super(root);
   }
 }
